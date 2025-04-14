7175e5f2378d476fed580dd14ebfd9ae10f70b69
felipe-coletti <felipecoletti2@gmail.com>
2025-04-14 12:08:47 -0300

    feat(sixth-semester/data-mining/class-001): add TAB1 exercise-002

--- sixth-semester/data-mining/class-001/exercise-002/query.sql
+++ sixth-semester/data-mining/class-001/exercise-002/query.sql
@@ -1,3 +1,6 @@
+CREATE TABLE TAB1 AS SELECT e.last_name, d.department_name FROM employees e
+JOIN departments d ON (e.department_ID=d.department_ID);
+
 CREATE TABLE TAB2 AS SELECT d.department_name, l.city FROM employees e 
 JOIN departments d ON e.department_id = d.department_id JOIN locations l ON 
 d.location_id = l.location_id WHERE e.job_id = 'ST_CLERK'; 
